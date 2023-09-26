a05fdd248b06c1d16e0cc2648ebb042bbea461c6
Manhdoan2004 <146045144+Manhdoan2004@users.noreply.github.com>
2023-09-26 08:33:24 +0700

    Add files via upload

---
+++ workspace/manh/Syntax/src/basic/dev/MainApp.java
@@ -0,0 +1,16 @@
+package basic.dev;
+
+public class MainApp {
+	//*comment 1 dong cach 1.Phim nong Ctrl	+/
+	/*comment 1 dong cach 2*/
+	public static void main(String[] args) {
+			System.out.println("Hello World");
+			int number;
+			String nameSchool;
+			
+	}
+/*Ten phuong thuc tuan theo quy tac camelCase*/
+	public void show() {
+
+}
+}
---
+++ workspace/manh/Variable/src/basic/dev/MainApp.java
@@ -0,0 +1,25 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+	/**
+	 * Khai bao bien
+	 */
+		String firtName = "Pham Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Ho va Ten");
+		System.out.println(old);
+		System.out.println(firtName+""+lastName);
+		System.out.println("Mark Math:"+markMath);
+		System.out.println("Passed:"+passed);
+		
+		final float PI=3.14f;
+		
+	}
+
+}
